69477e789d2b8b3ca5c2f6e5cec33310566cd1e2
hashdice <hashdice@hotmail.com>
2019-03-05 09:49:43 +0800

    range roll.

--- migrations/6_hashdice_vena.js
+++ migrations/6_hashdice_vena.js
@@ -2,5 +2,5 @@
 var dice_vena = artifacts.require("./HashDice_VENA.sol");
 
 module.exports = function(deployer) {
-  //deployer.deploy(dice_vena);
+  deployer.deploy(dice_vena);
 };
--- tronbox.js
+++ tronbox.js
@@ -3,9 +3,9 @@ const config = require('./config.js');
 module.exports = {
   networks: {    
     mainnet: {
-      from: config.environments.mainnet.address,
-      privateKey: config.environments.mainnet.private_key,
-      consume_user_resource_percent: 1,
+      from: 'your address',
+      privateKey: 'your private key',
+      consume_user_resource_percent: 5,
       fee_limit: 10000000,    //10 trx
       origin_energy_limit: 10000000,  //10 trx
       host: "https://api.trongrid.io",
@@ -15,37 +15,11 @@ module.exports = {
       eventServer: "https://api.trongrid.io",
       network_id: "*" // Match any network id
     }, 
-    hdt: {
-      from: config.environments.hdt.address,
-      privateKey: config.environments.hdt.private_key,
-      consume_user_resource_percent: 50,
-      fee_limit: 100000000,    //100 trx
-      origin_energy_limit: 10000000, //10 trx
-      host: "https://api.trongrid.io",
-      port: 8090,
-      fullNode: "https://api.trongrid.io",
-      solidityNode: "https://api.trongrid.io",
-      eventServer: "https://api.trongrid.io",
-      network_id: "*" 
-    },
-    hashdice: {
-      from: config.environments.hashdice.address,
-      privateKey: config.environments.hashdice.private_key,
-      consume_user_resource_percent: 10,
-      fee_limit: 100000000,    //100 trx
-      origin_energy_limit: 10000000, //10 trx
-      host: "https://api.trongrid.io",
-      port: 8090,
-      fullNode: "https://api.trongrid.io",
-      solidityNode: "https://api.trongrid.io",
-      eventServer: "https://api.trongrid.io",
-      network_id: "*" 
-    },
     shasta: {
-      from: config.environments.shasta.address,
-      privateKey: config.environments.shasta.private_key,
+      from: 'your address',
+      privateKey: 'your private key',
       consume_user_resource_percent: 5,
-      fee_limit: 100000000,    //100 trx
+      fee_limit: 10000000,    //10 trx
       origin_energy_limit: 10000000, //10 trx
       host: "https://api.shasta.trongrid.io",
       port: 8090,
